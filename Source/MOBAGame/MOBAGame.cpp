a6c0971b89f324c2b50526a26126988f982c3976
N0XIRE <connorwilson989@gmail.com>
2020-03-31 01:50:40 -0500

    Initial Menu

--- Source/MOBAGame/MOBAGame.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
-
-#include "MOBAGame.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MOBAGame, "MOBAGame" );
- 
