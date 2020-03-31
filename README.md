00cb9f27ea68595c3cdf1886ea66dda80112a6e3
zExcel <clarkjustin246@gmail.com>
2020-03-30 21:36:30 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MOBAGame
+
+Developed with Unreal Engine 4
---
+++ Source/MOBAGame/MOBAGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MOBAGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMOBAGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMOBAGameGameMode();
+};
+
+
+
